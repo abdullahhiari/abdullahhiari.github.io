df7a742d2e93cca2b97fa0a98c16f176284bd608
abdullahhiari <146450336+abdullahhiari@users.noreply.github.com>
2024-02-01 12:36:58 +0300

    Add files via upload

---
+++ final.js
@@ -0,0 +1,5 @@
+document.getElementById("contactForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("Thank you for contacting us! We will get back to you shortly.");
+    // Additional logic for handling form submission can be added here
+});
